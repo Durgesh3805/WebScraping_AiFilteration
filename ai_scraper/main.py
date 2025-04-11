69658c41110dd3a91beff017f7f298f49fb4382c
DurgeshAP <durgesh3805@gmail.com>
2025-04-11 14:23:28 +0530

    Final Starter repo

--- ai_scraper/main.py
+++ ai_scraper/main.py
@@ -1,11 +1,11 @@
-# from job_scraper import start_scraper
+from job_scraper import start_scraper
 # from filter import run_filtering
 
-# if __name__ == "__main__":
-#     start_scraper()      # Runs all spiders dynamically
-#     run_filtering()      # Runs filtering after scraping
+if __name__ == "__main__":
+    start_scraper()      # Runs all spiders dynamically
+    # run_filtering()      # Runs filtering after scraping
 
 
-from utils.db import init_db
+# from utils.db import init_db
 
-init_db()  # Ensure the 'jobs' table exists before any scraping
+# init_db()  # Ensure the 'jobs' table exists before any scraping
