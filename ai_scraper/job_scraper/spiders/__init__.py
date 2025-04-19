c9b4e9d47d0c7ea0d0ea04e21853d8a6cd0b39ab
Keerthana430 <keerthanakrisuj@gmail.com>
2025-04-19 16:33:16 +0530

    finalised stater file

--- ai_scraper/job_scraper/spiders/__init__.py
+++ ai_scraper/job_scraper/spiders/__init__.py
@@ -1,13 +1,12 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
-from job_scraper.spiders.quotes_spider import QuotesSpider
 # from job_scraper.spiders.another_spider import AnotherSpider  # Add more as needed
 
 def run_all_spiders():
     process = CrawlerProcess(settings=get_project_settings())
 
-    process.crawl(QuotesSpider)
+
     # process.crawl(AnotherSpider)
     # Add more crawlers here dynamically or use config for automation
 
