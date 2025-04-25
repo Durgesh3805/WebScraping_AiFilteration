f26ab5b41a4b0e423a29dd27c4736200850cd0f3
Durgesh A P <durgesh3805@gmail.com>
2025-04-25 21:11:10 +0530

    Created database for fetched jobs

--- ai_scraper/job_scraper/__init__.py
+++ ai_scraper/job_scraper/__init__.py
@@ -2,6 +2,6 @@ from job_scraper.spiders import run_all_spiders
 from utils import push_all_jsons_to_db
 def start_scraper():
     print("[Scraper] Starting spiders...")
-    # run_all_spiders()
+    run_all_spiders()
     print("[Scraper] Finished scraping. Inserting into DB...")
-    push_all_jsons_to_db()
+    # push_all_jsons_to_db()
