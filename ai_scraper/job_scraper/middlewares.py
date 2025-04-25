51094b208f9e6ce438d7df9bd281e14f45faf2c3
Durgesh A P <durgesh3805@gmail.com>
2025-04-25 19:53:04 +0530

    Few unchecked features

--- ai_scraper/job_scraper/middlewares.py
+++ ai_scraper/job_scraper/middlewares.py
@@ -101,3 +101,5 @@ class JobScraperDownloaderMiddleware:
 
     def spider_opened(self, spider):
         spider.logger.info("Spider opened: %s" % spider.name)
+
+
--- ai_scraper/job_scraper/spiders/__init__.py
+++ ai_scraper/job_scraper/spiders/__init__.py
@@ -2,15 +2,19 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
 # from job_scraper.spiders.internshala import InternshalaSpider
-from job_scraper.spiders.Freshersworld import FreshersworldSpider
+# from job_scraper.spiders.Freshersworld import FreshersworldSpider
+# from job_scraper.spiders.jobspresso import JobspressoSpider
+from ai_scraper.job_scraper.spiders.naukri_scraper import NaukriSpider
 # from job_scraper.spiders.quotes_spider import QuotesSpider
 # from job_scraper.spiders.another_spider import AnotherSpider  # Add more as needed
 
 
 def run_all_spiders():
     process = CrawlerProcess(settings=get_project_settings())
+    # process.crawl(JobspressoSpider)
+    process.crawl(NaukriSpider)
     # process.crawl(InternshalaSpider)
-    process.crawl(FreshersworldSpider)
+    # process.crawl(FreshersworldSpider)
     # process.crawl(QuotesSpider)/
     # process.crawl(AnotherSpider)
     # Add more crawlers here dynamically or use config for automation
